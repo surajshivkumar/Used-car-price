4cd51e5577dd02eefd8ee5e191052570f943e0a8
mukeshwar9 <mukeshwar1998@gmail.com>
2023-10-17 09:36:44 +0530

    index html boilerplate

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask , render_template,request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/",methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', val = {'key': 'hello'})
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+	app.run(debug=True)
