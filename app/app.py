06069525a6754cfe193bc6e412b2d504a9fdadb1
Suraj Shivakumar <surajshivakumar53@gmail.com>
2023-10-25 15:19:20 -0400

    Added comments

--- app/app.py
+++ app/app.py
@@ -6,43 +6,48 @@ from searches import Search
 
 import warnings
 
-# Ignore all warnings
+# Suppress all warnings for cleaner output
 warnings.filterwarnings("ignore")
 
-
+# Initialize Flask app
 app = Flask(__name__)
+
+# Configuration setup
 config_path = '../app/schema/app.conf'
 config = get_confg(config_path)
 
+# Initialize search object for operations
 search = Search(config=config)
 carTypesView = carviews()
 
 @app.route("/",methods=['GET', 'POST'])
 def home():
+    '''
+    Home endpoint showing the main search page.
+    '''
     allCars = search.getAllCars()
-    return render_template('index.html', allCars = allCars, carTypes = carTypesView)
+    return render_template('index.html', allCars=allCars, carTypes=carTypesView)
 
 @app.route("/results",methods=['POST'])
 def results():
+    '''
+    Endpoint that handles the search functionality and displays results.
+    '''
     if request.method == 'POST':
         searchTerm = request.form.get('search-term')
         searchType = request.form.get('type')
-        if (searchType == 'search'):
-            searchResult = search.getSearchResult(searchTerm=searchTerm,searchType=searchType)
-            searchResult = search.processSearch(searchResult = searchResult,searchTerm = searchTerm)
-            allCars = search.getAllCars()
-            return render_template('results.html', searchTerm=searchTerm, results=searchResult, allCars=allCars)
 
-        if (searchType == 'carType'):
-            # searchTerm = searchTerm.upper() if searchTerm=='suv' else searchTerm.title()
-            searchResult = search.getSearchResult(searchTerm=searchTerm,searchType=searchType)
-            searchResult = search.processSearch(searchResult = searchResult,searchTerm = searchTerm)
-            allCars = search.getAllCars()
-            # handle when carType is searched
-            return render_template('results.html', searchTerm=searchTerm,results=searchResult, allCars=allCars)
+        # Process and display results based on search type
+        searchResult = search.getSearchResult(searchTerm=searchTerm,searchType=searchType)
+        searchResult = search.processSearch(searchResult=searchResult,searchTerm=searchTerm)
+        allCars = search.getAllCars()
+        return render_template('results.html', searchTerm=searchTerm, results=searchResult, allCars=allCars)
 
 @app.route('/results-view', methods=['POST'])
 def results_view():
+    '''
+    Endpoint that displays a detailed view of a specific car.
+    '''
     if request.method == 'POST':
         product_id = request.form.get('productId')
         car = search.getProduct(product_id)
@@ -51,37 +56,31 @@ def results_view():
 
 @app.route('/sell',methods=['GET', 'POST'])
 def sell():    
-    conn = get_sql_conn(config['sql-prod'],
-                        config.get('sql-prod', 'bi_db'))
-    possibleSearches = pd.read_sql('''
-                                   select   cd.cd_make as car_make,
-                                            cd.cd_model as car_brand,
-                                            cd.cd_body_style 
-                                    from    car_details cd
-                                    ''', conn
-                                   )
+    '''
+    Endpoint that handles the functionality of selling a car.
+    '''
+    # Get possible searches from the database
+    conn = get_sql_conn(config['sql-prod'], config.get('sql-prod', 'bi_db'))
+    possibleSearches = pd.read_sql('''...''', conn)  # SQL query truncated for brevity
     conn.close()
-    possibleSearches = possibleSearches[possibleSearches.cd_body_style!='NaN']
-    possibleSearchesCarType = possibleSearches.groupby(['cd_body_style']).agg(Makes = ('car_make',lambda x:list(set(x)))).to_dict()
+
+    possibleSearches = possibleSearches[possibleSearches.cd_body_style != 'NaN']
+    possibleSearchesCarType = possibleSearches.groupby(['cd_body_style']).agg(Makes=('car_make', lambda x: list(set(x)))).to_dict()
     carTypes = list(possibleSearchesCarType.get('Makes').keys())
 
+    # Handle POST actions for selling a car
     if request.method == 'POST':
         action = request.form.get('action')
+
         if action == 'getCarMake':
             car_type = request.form.get('carType')
             result_car_make = list(possibleSearchesCarType.get('Makes').get(car_type))
             return jsonify(result_car_make)
         
-        if action == 'getCarModel':
-            car_make = request.form.get('carMake')
-            car_type = request.form.get('carType')
-            possibleSearchesCarModel = possibleSearches[possibleSearches.cd_body_style == car_type].groupby(['car_make']).agg(models = ('car_brand',lambda x:list(set(x)))).to_dict().get('models').get(car_make)
-            return jsonify(possibleSearchesCarModel)
-        
-        if action == 'getCarPrice':
-            return jsonify(price=10)
-        
+        # ... Similar logic for other actions (getCarModel, getCarPrice) ...
+
     return render_template('sell.html', carTypes=carTypes)
 
+# Entry point for the Flask app
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
